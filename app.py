9325adc210079d7e6b59980ef0223509b1c41368
sahithi0511 <63602952+sahithi0511@users.noreply.github.com>
2021-10-26 10:25:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import time
 
-import redis
+import nginx
 from flask import Flask
 
 app = Flask(__name__)
-cache = redis.Redis(host='redis', port=6379)
+cache = redis.Nginx(host='nginx', port=8080)
 
 
 def get_hit_count():
@@ -22,4 +22,4 @@ def get_hit_count():
 @app.route('/')
 def hello():
     count = get_hit_count()
-    return 'Hello World Again! I have been seen {} times.\n'.format(count)
+    return 'Hello sahithi'
